32c8329065399b42e95335fc18b8f82d0029379a
CodeMiner-掘金安东尼 <729922845@qq.com>
2026-04-08 10:22:19 +0800

    FIX：config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const sharedHeaders = {
-  'Cross-Origin-Embedder-Policy': 'require-corp',
-  'Cross-Origin-Opener-Policy': 'same-origin'
-}
-
 export default defineConfig({
   base: '/',
   plugins: [vue()],
   server: {
     port: 3000,
-    open: true,
-    headers: sharedHeaders
-  },
-  preview: {
-    headers: sharedHeaders
+    open: true
   }
-})
+}) 
